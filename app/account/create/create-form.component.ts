6ceceb62e6aa52e59a0da172cbe7444edb10395d
Matheus Mariano <vmatheus.mariano@gmail.com>
2016-04-14 21:14:51 -0300

    Allow submit account creation form

--- app/account/create/create-form.component.ts
+++ app/account/create/create-form.component.ts
@@ -1,46 +1,70 @@
 import { Component, OnInit } from 'angular2/core'
 import { ControlGroup, FormBuilder, Validators } from 'angular2/common'
+import { Router } from 'angular2/router'
 
 import { Validators as CtValidators } from '../../core/validators/validators'
+import { AccountService } from '../account.service'
 import { UsernameService } from '../username.service'
 import { EmailService } from '../email.service'
+import { CreateForm } from './create-form'
 
 @Component({
     selector: 'ct-create-form',
     templateUrl: 'app/account/create/create-form.component.html',
-    providers: [UsernameService, EmailService],
+    providers: [AccountService, UsernameService, EmailService],
 })
 export class CreateFormComponent implements OnInit {
     form: ControlGroup
+    model: CreateForm
 
     constructor(
+        private _router: Router,
         private _formBuilder: FormBuilder,
+        private _accountService: AccountService,
         private _usernameService: UsernameService,
         private _emailService: EmailService
     ) {}
 
     ngOnInit() {
-        this.applyValidators()
+        this._initModel()
+        this._applyValidators()
     }
 
-    applyValidators(): void {
+    submit(): void {
+        this._accountService
+            .store(this.model)
+            .subscribe((response) => {
+                this._router.navigate(['Index'])
+            })
+    }
+
+    private _initModel(): void {
+        this.model = {
+            name: '',
+            username: '',
+            email: '',
+            password: '',
+        }
+    }
+
+    private _applyValidators(): void {
         this.form = this._formBuilder.group({
-            name: ['', Validators.compose([
+            name: [this.model.name, Validators.compose([
                 Validators.required,
                 Validators.maxLength(255),
             ])],
-            username: ['', Validators.compose([
+            username: [this.model.username, Validators.compose([
                 Validators.required,
                 Validators.minLength(3),
                 Validators.maxLength(30),
                 Validators.pattern('[A-Za-z0-9-_]+'),
             ]), CtValidators.unique(this._usernameService)],
-            email: ['', Validators.compose([
+            email: [this.model.email, Validators.compose([
                 Validators.required,
                 Validators.maxLength(255),
                 CtValidators.email,
             ]), CtValidators.unique(this._emailService)],
-            password: ['', Validators.compose([
+            password: [this.model.password, Validators.compose([
                 Validators.required,
                 Validators.minLength(6),
                 Validators.maxLength(255),
---
+++ app/account/create/create-form.ts
@@ -0,0 +1,6 @@
+export interface CreateForm {
+    name: string,
+    username: string,
+    email: string,
+    password: string,
+}
