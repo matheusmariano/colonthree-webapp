f08726e1571059028b9998c3fef791de12fcea38
Matheus Mariano <vmatheus.mariano@gmail.com>
2016-04-16 12:29:19 -0300

    Add titles to components

--- app/components/home/index/index.component.ts
+++ app/components/home/index/index.component.ts
@@ -1,13 +1,22 @@
+import { Title } from 'angular2/platform/browser'
 import { Component } from 'angular2/core'
-import { Router } from 'angular2/router'
+import { OnActivate, Router } from 'angular2/router'
 
 import { Navigable } from '../../../common/router/navigable'
 
 @Component({
     templateUrl: 'app/components/home/index/index.component.html',
+    providers: [Title],
 })
-export class IndexComponent implements Navigable {
-    constructor(private _router: Router) {}
+export class IndexComponent implements OnActivate, Navigable {
+    constructor(
+        private _title: Title,
+        private _router: Router
+    ) {}
+
+    routerOnActivate() {
+        this._title.setTitle('ColonThree')
+    }
 
     navigate(params) {
         this._router.navigate(params)
--- app/components/home/register/register.component.ts
+++ app/components/home/register/register.component.ts
@@ -1,9 +1,20 @@
+import { Title } from 'angular2/platform/browser'
 import { Component } from 'angular2/core'
+import { OnActivate } from 'angular2/router'
 
 import { RegisterFormComponent } from './register-form.component'
 
 @Component({
     templateUrl: 'app/components/home/register/register.component.html',
     directives: [RegisterFormComponent],
+    providers: [Title],
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnActivate {
+    constructor(
+        private _title: Title
+    ) {}
+
+    routerOnActivate() {
+        this._title.setTitle('Register - ColonThree')
+    }
+}
