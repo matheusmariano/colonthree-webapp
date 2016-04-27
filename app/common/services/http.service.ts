6c7ffb25fbcff78b8b163653bcfa8e350b5f4ec3
Matheus Mariano <vmatheus.mariano@gmail.com>
2016-04-26 21:59:25 -0300

    Update HTTP Service
    Allow insert headers into methods.
    Fix error handling when the response has no metadata.

--- app/common/services/http.service.ts
+++ app/common/services/http.service.ts
@@ -7,10 +7,18 @@ import { Config } from '../config'
 export abstract class HttpService {
     constructor(private _http: Http) {}
 
-    post(method: string, body: string): Observable<any> {
+    post(
+        method: string,
+        body: string,
+        headers: { [key: string]: string } = {}
+    ): Observable<any> {
         let uri: string = Config.apiBaseUrl + method
-        let headers = new Headers({ 'Content-Type': 'application/json' })
-        let options = new RequestOptions({ headers })
+
+        headers['Content-Type'] = 'application/json'
+
+        let options = new RequestOptions({
+            headers: new Headers(headers),
+        })
 
         return this._http
             .post(uri, body, options)
@@ -18,10 +26,15 @@ export abstract class HttpService {
             .catch(this._handleError)
     }
 
-    private _handleError(error: Response) {
-        let meta: { status: number, message: string } = error.json().meta
-        let message: string = `${meta.status} ${meta.message}`
+    private _handleError(response: Response) {
+        let error = response.json()
+        let message: string = 'Server Error'
 
-        return Observable.throw(message || 'Server Error')
+        if (error.meta) {
+            let meta: { status: number, message: string } = error.meta
+            message = `${meta.status} ${meta.message}`
+        }
+
+        return Observable.throw(message)
     }
 }
